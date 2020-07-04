85511e64248868b38f392e46fed7c2967eb7d59b
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-07-04 13:10:40 +0300

    added some variables to the javascript file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.log('it works');
 
 // code your solution in here
+
+const titleNameInput = document.querySelector("#new-post-title");
+const contentNameInput = document.querySelector("#new-post-content");
+const submitButton = document.querySelector("#submit-form");
+const postList = document.querySelector("#post-list");
+
